56543f77094ef83964858c14bd2755378b5444d4
xVal <valentinocarneirogoncalves@gmail.com>
2021-07-04 15:02:08 +0100

    .

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pp()-&cu^ptq0hp=_-e1&nhb@07q#%d$=afe&9g1m#&6f05et+'
+SECRET_KEY = 'Haha.There is nothing here to you :D'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
